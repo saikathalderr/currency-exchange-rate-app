bd82cf06d86c322d4b6f38083fb9aa80fbf0ad7d
Saikat Halder <saikat.halder@lotto24.de>
2024-12-08 18:57:09 +0100

    created CurrencySelector UI component

---
+++ src/components/ui/CurrencySelector.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Flag from "@components/ui/Flag.vue";
+
+const props = defineProps({
+  code: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["click"]);
+</script>
+
+<template>
+  <div
+    class="inline-flex justify-center items-center p-2 cursor-pointer"
+    @click="emit('click')"
+  >
+    <Flag :name="props.code" :icon="props.icon" />
+    <span class="text-black font-bold ml-1"> {{ props.code }} </span>
+    <img src="@/assets/icons/dropdown.svg" alt="dropdown" />
+  </div>
+</template>
