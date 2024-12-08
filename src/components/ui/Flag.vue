0dbcdeec2be8593003f3d5b0a80f169b08de6b18
Saikat Halder <saikat.halder@lotto24.de>
2024-12-08 18:22:33 +0100

    created Flag UI component

---
+++ src/components/ui/Flag.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { PropType } from "vue";
+
+const props = defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String as PropType<"sm" | "md" | "lg">,
+    default: "sm",
+  },
+});
+
+const flagSizeClass = {
+  sm: "w-5 h-5",
+  md: "w-8 h-8",
+  lg: "w-12 h-12",
+};
+
+const flagClass = `rounded-full ${flagSizeClass[props.size]}`;
+</script>
+
+<template>
+  <img :src="props.icon" :alt="props.name.toLowerCase()" :class="flagClass" />
+</template>
