1d675db815edcaa900d2f3c4cd1ff6236609e610
Saikat Halder <saikat.halder@lotto24.de>
2024-12-13 17:57:51 +0100

    created CurrencySearch UI component

---
+++ src/components/ui/CurrencySearch.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const search = defineModel("search", {
+  type: String,
+  required: true,
+  default: "",
+});
+</script>
+
+<template>
+  <input
+    type="text"
+    class="w-full p-2 text-sm outline-none bg-gray-50"
+    v-model="search"
+    placeholder="Search for a currency"
+  />
+</template>
